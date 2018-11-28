c60cf68fc111beaca1eed1c6820d421f52f590d6
Jeremy Long <jeremy.long@gmail.com>
2018-11-28 06:59:08 -0500

    wip

--- ant/src/main/java/org/owasp/dependencycheck/taskdefs/Check.java
+++ ant/src/main/java/org/owasp/dependencycheck/taskdefs/Check.java
@@ -1457,7 +1457,7 @@ public class Check extends Update {
         final StringBuilder ids = new StringBuilder();
         for (Dependency d : dependencies) {
             for (Vulnerability v : d.getVulnerabilities()) {
-                if (v.getCvssScore() >= failBuildOnCVSS) {
+                if (v.getCvssV2Score() >= failBuildOnCVSS) {
                     if (ids.length() == 0) {
                         ids.append(v.getName());
                     } else {
