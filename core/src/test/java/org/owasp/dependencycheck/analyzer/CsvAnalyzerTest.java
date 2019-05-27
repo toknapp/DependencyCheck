277fc53c9219f9df809494468a8a06fa86497055
Gustav Behm <gustav@upvest.co>
2019-05-27 03:32:44 +0200

    Add an example CSV dependency file

--- core/src/test/java/org/owasp/dependencycheck/analyzer/CsvAnalyzerTest.java
+++ core/src/test/java/org/owasp/dependencycheck/analyzer/CsvAnalyzerTest.java
@@ -3,9 +3,12 @@ package org.owasp.dependencycheck.analyzer;
 import java.io.File;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.owasp.dependencycheck.BaseTest;
+import org.owasp.dependencycheck.Engine;
+import org.owasp.dependencycheck.dependency.Dependency;
 
 public class CsvAnalyzerTest extends BaseTest {
     @Test
@@ -18,5 +21,16 @@ public class CsvAnalyzerTest extends BaseTest {
         for (String name : files) {
             assertTrue(name, instance.accept(new File(name)));
         }
+    }
+
+    @Test
+    public void testSimple() throws Exception {
+        Engine engine = new Engine(getSettings());
+        Dependency dep = new Dependency(BaseTest.getResourceAsFile(this, "csv/dependencies.csv"));
+        CsvAnalyzer instance = new CsvAnalyzer();
+        instance.initialize(getSettings());
+        instance.analyze(dep, engine);
+        Dependency[] dependencies = engine.getDependencies();
+        assertEquals("number of dependencies should be 1", 1, dependencies.length);
     }
 }
